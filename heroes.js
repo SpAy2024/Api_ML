13dfebe9bfb9c37b41fcfec9ed270b2338f593e0
SpAy2024 <alexvillanuevasoto@gmail.com>
2025-08-27 16:55:49 -0400

    Update heroes.js

--- heroes.js
+++ heroes.js
@@ -1,76 +1,98 @@
 const heroes = [
   {
     id: 1,
-    nombre: "Alucard",
-    rol: "Fighter",
-    winRate: "52%",
-    imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/homepage/100_262dcfb1e67c19e273ac2409098c8b5d.png",
+    nombre: "Miya",
+    rol: "Marksman",
+    winRate: "49%",
+    imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/homepage/100_32c0d9d3a727a9052754296af6251435.png",
+    icon: "https://akmweb.youngjoygame.com/web/gms/image/025c69a764924f4bac526a2662f1a0b9.png",
+    guia: "https://img.mobilelegends.com/group1/M00/00/BB/rBEABWWBg0iAEVjjAAC2G6fDQV8498.jpg",
     skills: [
       {
-        nombre: "Groundsplitter",
+        nombre: "Bendición de la luna",
         descripcion: "Alucard salta hacia adelante y causa daño físico a los enemigos.",
-        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/skill/alucard_groundsplitter.png"
+        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/homepage/100_fbe01740efd779f6059fd2313b427457.png"
       },
       {
-        nombre: "Whirling Smash",
+        nombre: "Flecha de luna",
         descripcion: "Alucard golpea en círculo causando daño a los enemigos cercanos.",
-        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/skill/alucard_whirlingsmash.png"
+        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/homepage/100_de41566ded345f89064e32da9dfd8893.png"
       },
       {
-        nombre: "Fission Wave",
+        nombre: "Flecha del eclipse",
         descripcion: "Lanza una onda de energía hacia adelante causando gran daño.",
-        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/skill/alucard_fissionwave.png"
+        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/homepage/100_c5771dd5686278640dabfaa26839545c.png"
+      },
+      {
+        nombre: "Luz de luna oculta",
+        descripcion: "Lanza una onda de energía hacia adelante causando gran daño.",
+        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/homepage/100_361546d795e6df7029a1cf1252e57ac8.png"
       }
     ]
   },
   {
     id: 2,
-    nombre: "Miya",
+    nombre: "Balmond",
     rol: "Marksman",
-    winRate: "48%",
-    imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/homepage/101_7d31e1d8af5e4e41f60be1c3981c9b3a.png",
+    winRate: "52%",
+    imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/homepage/100_774d5cdd7ab9bed0afc7e11dc9d1c97d.png",
+    icon: "https://akmweb.youngjoygame.com/web/gms/image/629e282165d4b63deceaf350426ea440.png",
+    guia: "https://img.mobilelegends.com/group1/M00/00/BB/rBEABWWBg0iAKWmEAADHGo6-9Ls758.jpg",
     skills: [
       {
-        nombre: "Fission Shot",
+        nombre: "Sed de sangre",
         descripcion: "Miya dispara flechas dobles, causando más daño.",
-        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/skill/miya_fissionshot.png"
+        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/homepage/100_a9aa00dcaf998c9636e1dbb874a225c5.png"
       },
       {
-        nombre: "Rain of Arrows",
+        nombre: "Bloqueo del alma",
         descripcion: "Dispara una lluvia de flechas que ralentiza y daña a los enemigos.",
-        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/skill/miya_rainofarrows.png"
+        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/homepage/100_8bfe9cffa391a97995c46977e95d4be8.png"
       },
       {
-        nombre: "Turbo Stealth",
+        nombre: "Barrido ciclónico",
         descripcion: "Miya se vuelve invisible y aumenta su velocidad de ataque.",
-        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/skill/miya_turbostealth.png"
+        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/homepage/100_5120ddf10175a28628b37ddb846df631.png"
+      },
+      {
+        nombre: "Contraataque letal",
+        descripcion: "Miya se vuelve invisible y aumenta su velocidad de ataque.",
+        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/homepage/100_0a7365234448da509cb88a0baf2b92dd.png"
       }
     ]
   },
   {
     id: 3,
-    nombre: "Eudora",
-    rol: "Mage",
-    winRate: "55%",
-    imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/homepage/102_5a5a1a3cf6a724b456f25c759e6fef11.png",
+    nombre: "Sable",
+    rol: "Asesino",
+    winRate: "49.7%",
+    imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/homepage/100_75f50285862c6c34aa1770b11d00b4f8.png",
+    icon: "https://akmweb.youngjoygame.com/web/gms/image/d0b8b65a47fc43dc7bb2bac447072fd2.png",
+    guia: "https://img.mobilelegends.com/group1/M00/00/BB/rBEABWWBg0uANltIAAC78dciNUQ747.jpg",
     skills: [
       {
-        nombre: "Forked Lightning",
+        nombre: "La perdición del enemigo",
         descripcion: "Eudora lanza rayos hacia adelante causando daño mágico.",
-        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/skill/eudora_forkedlightning.png"
+        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/homepage/100_1ec282b532be7af31153ce75b206f112.png"
       },
       {
-        nombre: "Electric Arrow",
+        nombre: "Espada en órbita",
         descripcion: "Lanza un rayo que aturde al objetivo.",
-        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/skill/eudora_electricarrow.png"
+        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/homepage/100_c6294839bbadfa6f203cd57bdc15efe2.png"
       },
       {
-        nombre: "Thunderstruck",
+        nombre: "Cargar",
         descripcion: "Invoca un rayo masivo que inflige gran daño mágico.",
-        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/skill/eudora_thunderstruck.png"
+        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/homepage/100_8607d91bbdff281d5bba7618f3ed4c98.png"
+      },
+      {
+        nombre: "Triple barrido",
+        descripcion: "Invoca un rayo masivo que inflige gran daño mágico.",
+        imagen: "https://akmweb.youngjoygame.com/web/svnres/img/mlbb/homepage/100_fc47930fd50e062bf6e8c8c0398e237c.png"
       }
     ]
   }
 ];
 
 module.exports = heroes;
+
